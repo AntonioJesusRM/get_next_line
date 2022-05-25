499c6f826de287fc5808b2c8682cace1fc0b5221
Antonio jesus Ruiz moreno <aruiz-mo@c1r9s5.42malaga.com>
2022-05-25 13:04:20 +0200

    get_next_line 125%

---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aruiz-mo <aruiz-mo@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/13 12:11:49 by aruiz-mo          #+#    #+#             */
+/*   Updated: 2022/05/13 12:12:18 by aruiz-mo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <stdlib.h>
+# include <unistd.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+unsigned int	ft_strlen(const char *s);
+int				ft_line_len(char *ptr);
+void			*ft_memcpy(void *dst, const void *src, unsigned int n);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strjoin(char const *s1, char const *s2);
+void			ft_clean_memory(char **memory);
+int				ft_fill_memory(char **memory, int fd);
+char			*ft_get_line(char **memory);
+char			*get_next_line(int fd);
+
+#endif
